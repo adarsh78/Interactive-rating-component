03fcd5218b06886061eeb40b570b38ac21cd6637
Adarsh Gupta <64201509+adarsh78@users.noreply.github.com>
2023-02-19 16:24:43 +0530

    JS File
    JS File

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
-    $('.btn').click(function() {
-        $('.thankyou').toggle();
-    });
-});
+const mainCard = document.querySelector(".main-card");
+const thankyouCard = document.querySelector(".thankyou-card");
+const ratings = document.querySelectorAll(".btn");
+const submitButton = document.getElementById("btn-submit-id");
+const finalRating = document.getElementById("rate");
+
+ratings.forEach((rate) => {
+    rate.addEventListener("click", () => {
+
+      finalRating.innerHTML = rate.innerHTML
+    })
+  })
+
+submitButton.addEventListener("click", () => {
+    mainCard.style.display = "none"
+    thankyouCard.classList.remove("thankyou-card-hidden")
+})
+
